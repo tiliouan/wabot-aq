ca22517b823cd2f645cb987aac9f5f1ce87c79e2
tiliouan <141184747+tiliouan@users.noreply.github.com>
2024-04-10 23:09:25 +0000

    Update resetuser.js

--- resetuser.js
+++ resetuser.js
@@ -41,7 +41,7 @@ let handler = async (m, { conn, text }) => {
  
  }
 }
-handler.help = ['reset *628xxxxxxxxxxx*']
+handler.help = ['reset *212xxxxxxxxxxx*']
 handler.tags = ['owner']
 handler.command = /^reset$/i
 handler.admin = false
